b70e08f39307bf8c48495f0c4f398979f8274420
jmzii <joonaskarkkainenn@gmail.com>
2024-02-09 03:59:52 +0200

    Added functionality for making appointments

--- frontend/src/MakeAppointment.jsx
+++ frontend/src/MakeAppointment.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MakeAppointment = () => {
   const [selectedDate, setSelectedDate] = useState("");
@@ -12,8 +14,69 @@ const MakeAppointment = () => {
     "Occupational health physician",
   ];
 
+  const generateTimeOptions = () => {
+    const startTime = new Date();
+    startTime.setHours(8, 0, 0);
+
+    const endTime = new Date();
+    endTime.setHours(20, 0, 0);
+
+    const timeOptions = [];
+
+    while (startTime <= endTime) {
+      const formattedTime = startTime.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+      });
+      timeOptions.push(
+        <option key={formattedTime} value={formattedTime}>
+          {formattedTime}
+        </option>
+      );
+
+      startTime.setMinutes(startTime.getMinutes() + 15);
+    }
+
+    return timeOptions;
+  };
+
   const HandleAppointmentSubmit = () => {
-    // tähän logiikka
+    // insert logic here plz
+    if (!selectedDate || !selectedTime || !selectedDoctor) {
+      console.error("Make sure to fill all fields!");
+      toast.error("Appointment failed. Make sure to fill all fields!");
+      return;
+    }
+
+    const appointmentData = {
+      appointmentdate: selectedDate,
+      appointmenttime: selectedTime,
+      selecteddoctor: selectedDoctor,
+    };
+
+    // POST request to backend
+    fetch("http://localhost:3001/MakeAppointment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(appointmentData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // log response from server
+      })
+
+      .catch((error) => {
+        console.error("Error submitting appointment data to database: ", error);
+      });
+
+    toast.success("Appointment made succesfully!");
+
+    // clear all fields
+    setSelectedDate("");
+    setSelectedTime("");
+    setSelectedDoctor("");
   };
 
   return (
@@ -31,11 +94,15 @@ const MakeAppointment = () => {
         <br />
         <label>
           Time:
-          <input
-            type="time"
+          <select
             value={selectedTime}
             onChange={(e) => setSelectedTime(e.target.value)}
-          />
+          >
+            <option value="" disabled>
+              Choose a time
+            </option>
+            {generateTimeOptions()}
+          </select>
         </label>
         <br />
         <label>
@@ -59,6 +126,7 @@ const MakeAppointment = () => {
           Submit Appointment
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 };
